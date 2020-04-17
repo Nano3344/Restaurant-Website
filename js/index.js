6febb9e3796506e451c1d967e4b4c7f506928a0b
Malik Ebers <malik.ebers@web.de>
2020-04-17 18:59:39 +0200

    Added new Styling

---
+++ js/index.js
@@ -0,0 +1,62 @@
+(function (){
+  const cart = document.querySelector('.Cart');
+  const cartWindow = document.querySelector('.Cart-window');
+
+  cart.addEventListener('click', function() {
+    cartWindow.classList.toggle('Cart-show');
+  });
+})();
+
+(function () {
+  const addList = document.querySelectorAll('.order-now');
+
+  addList.forEach(function(add) {
+    add.addEventListener('click', function(event) {
+      if(event.target.classList.contains('order-now')) {
+        let path = event.target.previousElementSibling.previousElementSibling.src;
+
+        const item = {};
+
+        item.img = path;
+        let name = event.target.previousElementSibling.children[0].textContent;
+        item.name = name;
+        let price = event.target.previousElementSibling.children[1].textContent;
+        item.price = price;
+
+        const newCartItem = document.createElement('div');
+        newCartItem.classList.add('Cart-item');
+        newCartItem.innerHTML = `<img src="${item.img}"><div class="Cart-info"><p> ${item.name} </p> <p>${item.price}</p> </div> <span>X</span>`;
+
+        const carttotal = document.querySelector('.Cart-total');
+        const cartwindow = document.querySelector('.Cart-window');
+        cartwindow.insertBefore(newCartItem, carttotal);
+        alert(`${item.name} was added to the cart`);
+        totalValue();
+      }
+    });
+  });
+  function totalValue() {
+    const total = [];
+    const allPrices = document.querySelectorAll('.Food-price');
+
+
+    allPrices.forEach(function(item) {
+       total.push(parseFloat(item.textContent));
+       console.log(total);
+    });
+
+  }
+  const removeBtn = document.querySelectorAll('span');
+  removeBtn.forEach(function(remove){
+  remove.addEventListener('click', function(event) {
+     event.target.parentElement.remove();
+  });
+});
+
+  const clearBtn = document.querySelector('.Cart-clear');
+  const allItems = document.querySelectorAll('.Cart-item');
+
+  clearBtn.addEventListener('click', function() {
+       allItems.remove();
+  });
+})();
